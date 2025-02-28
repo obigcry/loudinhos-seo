6cdfc6e17b20ab1a5d4373e0ff6d5966cc47d753
isisnaldojoao <isisnaldojoao@gmail.com>
2025-02-28 20:42:09 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { useState } from 'react'
+import { ClipboardCheck } from 'lucide-react';
+import { Toaster, toast } from 'react-hot-toast';
+import Image from 'next/image';
+
+export default function App() {
+  const [tag, setTag] = useState("");
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState(0);
+  const [copied, setCopied] = useState(false);
+
+  const allTags = [
+    { id: '0', name: 'VALORANT', tags: '#valorant #vlr #vctamericas #vct #valorantchampions #valorantchampionstour #loud #loudgg #riot #riotgames #esports' },
+    { id: '1', name: 'LOL', tags: '#CBLOL #CBLOLACADEMY #LEAGUEOFLEGENDS #LOL #RIOT #RIOTGAMES #ESPORTS #loud #loudgg' },
+    { id: '2', name: 'FREEFIRE', tags: '#ff #freefire #freefirebrasil #ffbrasil #garena #garenabrasil #garenafreefire #garenaoficial #esports #loud #loudgg' },
+    { id: '3', name: 'GERAL', tags: '#valorant #vlr #vctamericas #vct #valorantchampions #valorantchampionstour #loud #loudgg #riot #riotgames #CBLOL #CBLOLACADEMY #LEAGUEOFLEGENDS #LOL #RIOT #RIOTGAMES #ff #freefire #freefirebrasil #ffbrasil #garena #garenabrasil #garenafreefire #garenaoficial #esports #loud #loudgg' },
+    { id: '4', name: 'INFLUENCERS', tags: '#loud #loudgg #twitch #twitchstreamer #twitchtv #youtube #youtubestream #stream #livestream' },
+  ];
+
+  function handleSwitchCategory(index: number) {
+    setCategoriaSelecionada(index);
+  }
+
+  function generatorSeo() {
+    setTag(allTags[categoriaSelecionada].tags);
+    toast.success("Hastags geradas com sucesso!");
+    setCopied(false); // Resetar o estado do botão
+  }
+
+  async function copyToClipboard() {
+    if (tag) {
+      await navigator.clipboard.writeText(tag);
+      toast.success("Hastags copiadas com sucesso!");
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Resetar depois de 2s
+      console.log(copied)
+    }
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className='flex flex-col items-center min-h-screen p-6'>
+      <nav>
+        <Image alt="inoticiama" src='https://loud.gg/cdn/shop/files/LOUD___Wordmark_White.png?v=1727203990&width=248' />
+      </nav>
+      <div><Toaster/></div>
+      <section className="flex flex-col m-20 text-xl items-center select-none  main-seo">
+        <section className="title-seo">
+          <h1>Gerador de SEO</h1>
+        </section>
+        <section className='flex flex-col md:flex-row gap-10 m-20 options-seo'>
+          {allTags.map((tags, index) => (
+            <div className='flex bg-white text-black p-4 border-rounded rounded-xl transition-all hover:scale-105 hover:bg-green-500 hover:text-white cursor-pointer' key={tags.id}>
+              <h1
+                className={`option-seo ${index === categoriaSelecionada ? 'selected' : ''}`}
+                onClick={() => handleSwitchCategory(index)}
+              >
+                {tags.name}
+              </h1>
+            </div>
+          ))}
+        </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <button className='cursor-pointer m-5  border-rounded  transition-all hover:border-b  hover:inline-block ' onClick={generatorSeo}>
+          Gerar SEO
+        </button>
+
+        <section className='flex flex-col md:flex-col bg-gray-700 p-10 mt-20  tags-seo border-rounded rounded-xl'>
+          {tag !== '' && (
+            <>
+              <p>{tag}</p>
+              <button onClick={copyToClipboard} className='flex items-center justify-center i cursor-pointer p-2 bg-gray-600 border-rounded rounded-xl w-[50px]'>
+                <ClipboardCheck className='items-center' />
+              </button>
+            </>
+          )}
+        </section>
+      </section>
+    </main>
   );
 }
